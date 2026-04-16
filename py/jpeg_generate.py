0d659f788b8ccebdd9fc5892129ab9dba6f086ee
chenguangzeng <chenguangzeng@cgz.com>
2026-04-16 15:00:21 +0800

    add r

--- py/jpeg_generate.py
+++ py/jpeg_generate.py
@@ -13,9 +13,9 @@ def jpeg_generate_from_bgr_raw():
         
     # 分离平面
     size = width * height
-    b_plane = bgr_data[0:size].reshape((height, width))
+    r_plane = bgr_data[0:size].reshape((height, width))
     g_plane = bgr_data[size:2*size].reshape((height, width))
-    r_plane = bgr_data[2*size:3*size].reshape((height, width))
+    b_plane = bgr_data[2*size:3*size].reshape((height, width))
 
     img = cv2.merge([b_plane, g_plane, r_plane])
     output_bgr_jpg_file = "/mnt/workspace/cgz_workspace/Exercise/opencv_example/output/frontwide_1920_1024_3.jpg"
